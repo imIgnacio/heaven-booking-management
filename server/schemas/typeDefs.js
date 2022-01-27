b0e292e1edfab9a9539ecd6bd773e3178d1355c5
Ignacio Diaz <nachodiaz99@gmail.com>
2022-01-27 15:09:05 +0400

    type defs done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,17 @@ const typeDefs = gql`
   type Room {
     _id: ID!
     name: String!
+    maxGuests: Int!
+    price: Int!
+    isEmpty: Boolean!
+    bookings: [Booking]
+  }
+
+  type Booking {
+    _id: ID!
+    checkInDate: Date!
+    checkOutDate: Date!
+    guest: Guest!
   }
 
   type User {
@@ -27,15 +38,18 @@ const typeDefs = gql`
     room: [Room]
     guest: [Guest]
     user: [User]
+    booking: [Booking]
     users(_id: String): [User]
     guests(_id: String): [Guest]
     rooms(_id: String): [Room]
+    bookings(_id: String): [Booking]
   }
 
   type Mutation {
     createUser(username: String!, email: String!, password: String!): User
     createGuest(firstName: String!, lastName: String!, phone: Int!, email: String!): Guest
     createRoom(name: String!, maxGuests: Int!, price: Int!): Room
+    createBooking(checkInDate: Date!, checkOutDate: Date!, guest: Guest!): Booking
   }
 `;
 
