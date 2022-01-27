330a04d61254890987e2ebc9190428f9fd14e16a
Ignacio Diaz <nachodiaz99@gmail.com>
2022-01-27 13:38:34 +0400

    user schema done

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,56 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require("bcryptjs")
 
-const matchupSchema = new Schema({
-  tech1: {
+const validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+return re.test(email)
+};
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [ validateEmail, 'invalid email' ],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+  },
+  password: {
     type: String,
     required: true,
   },
-  tech2: {
-    type: String,
-    required: true,
-  },
-  tech1_votes: {
-    type: Number,
-    default: 0,
-  },
-  tech2_votes: {
-    type: Number,
-    default: 0,
-  },
+  rooms: [{ type: Schema.Types.ObjectId, ref: 'Room'}],
+  guests: [{ type: Schema.Types.ObjectId, ref: 'Guest'}],
 });
 
-const Matchup = model('Matchup', matchupSchema);
+// Pre middleware to hash passwords
+userSchema.pre("save", function (next) {
+  const user = this
 
-module.exports = Matchup;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError)
+      } else {
+        bcrypt.hash(user.password, salt, function(hashError, hash) {
+          if (hashError) {
+            return next(hashError)
+          }
+
+          user.password = hash
+          next()
+        })
+      }
+    })
+  } else {
+    return next()
+  }
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
